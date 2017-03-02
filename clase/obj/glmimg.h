3883a0d8afdb1c41c8e90625ab31a67a13824b99
Tuffk <tuffkmulhall@gmail.com>
2017-03-02 15:54:52 -0600

    hola

---
+++ clase/obj/glmimg.h
@@ -0,0 +1,4 @@
+#ifndef _GLMIMG
+#define _GLMIMG
+GLuint glmLoadTexture(char *filename, GLboolean alpha, GLboolean repeat, GLboolean filtering, GLboolean mipmaps, GLfloat *texcoordwidth, GLfloat *texcoordheight);
+#endif

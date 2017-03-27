017694b6373d1b49f3dd9f11f2349be5b168d66f
Tuffk <tuffkmulhall@gmail.com>
2017-03-27 15:51:12 -0600

    done ovni

--- clase/surface.c
+++ clase/surface.c
@@ -1,9 +1,9 @@
 #include <GL/glut.h>
 
-GLfloat ctlpoints[4][4][3];
+GLfloat ctlpoints[4][4][3], points2[4][4][3];
 int showPoints = 0;
 
-GLUnurbsObj *theNurb;
+GLUnurbsObj *theNurb, *nurb2;
 float angX=0, angY=0;
 
 /*
@@ -19,11 +19,29 @@ void init_surface(void)
             ctlpoints[u][v][1] = 2.0*((GLfloat)v - 1.5);
 
             if ( (u == 1 || u == 2) && (v == 1 || v == 2))
-                ctlpoints[u][v][2] = 3.0;
+                ctlpoints[u][v][2] = -3.0;
             // else if (u == 0 || v== 0)
             //     ctlpoints[u][v][2] = -0.0;
             else
              ctlpoints[u][v][2] = -0.0;
+        }
+    }				
+}
+
+void init_surface2(void)
+{
+    int u, v;
+    for (u = 0; u < 4; u++) {
+        for (v = 0; v < 4; v++) {
+            points2[u][v][0] = 2.0*((GLfloat)u - 1.5);
+            points2[u][v][1] = 2.0*((GLfloat)v - 1.5);
+
+            if ( (u == 1 || u == 2) && (v == 1 || v == 2))
+                points2[u][v][2] = -3.0;
+            // else if (u == 0 || v== 0)
+            //     ctlpoints[u][v][2] = -0.0;
+            else
+             points2[u][v][2] = -0.0;
         }
     }				
 }				
@@ -40,19 +58,28 @@ void myinit(void)
     glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
     glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
     glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+    GLfloat pos1[] = {0,0,-6}, pos2[] = {0,0,6};
+
+    glLightfv(GL_LIGHT0,GL_POSITION,pos1);
+    glLightfv(GL_LIGHT1,GL_POSITION,pos2);
 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
     glDepthFunc(GL_LESS);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_AUTO_NORMAL);
     glEnable(GL_NORMALIZE);
 
     init_surface();
+    init_surface2();
 
     theNurb = gluNewNurbsRenderer();
     gluNurbsProperty(theNurb, GLU_SAMPLING_TOLERANCE, 25.0);
     gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
+    nurb2 = gluNewNurbsRenderer();
+    gluNurbsProperty(nurb2, GLU_SAMPLING_TOLERANCE, 25.0);
+    gluNurbsProperty(nurb2, GLU_DISPLAY_MODE, GLU_FILL);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -61,7 +88,7 @@ void myinit(void)
 
 void display(void)
 {
-    GLfloat knots[8] = {0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0};
+    GLfloat knots[8] = {0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0},knots2[8] = {0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0};
     int i, j;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -82,6 +109,32 @@ void display(void)
                 4, 4, // cnaitdad de puntos en v y u
                 GL_MAP2_VERTEX_3);// tipo de mapa y vertices
         gluEndSurface(theNurb);
+
+        //     gluBeginSurface(nurb2);
+        //     gluNurbsSurface(nurb2, 
+        //         8, knots2,
+        //         8, knots2,
+        //         4 * 3, //cantidad de puntos para ir saltando 
+        //         3, // cantidad de valores zyx
+        //         &points2[0][0][0], 
+        //         4, 4, // cnaitdad de puntos en v y u
+        //         GL_MAP2_VERTEX_3);// tipo de mapa y vertices
+        // gluEndSurface(nurb2);
+
+        glPushMatrix();
+            glScalef(-1,-1,-1);
+            gluBeginSurface(theNurb);
+            gluNurbsSurface(theNurb, 
+                8, knots,
+                8, knots,
+                4 * 3, //cantidad de puntos para ir saltando 
+                3, // cantidad de valores zyx
+                &ctlpoints[0][0][0], 
+                4, 4, // cnaitdad de puntos en v y u
+                GL_MAP2_VERTEX_3);// tipo de mapa y vertices
+        gluEndSurface(theNurb);
+        glPopMatrix();
+        
 
         if(showPoints) {
             glPointSize(5.0);

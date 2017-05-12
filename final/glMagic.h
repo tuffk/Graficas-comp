a98b1dd123f5439cc8ccfc60e9a2d6e11a637fd0
Tuffk <tuffkmulhall@gmail.com>
2017-05-12 13:03:36 -0500

    2 clases y bien mamonas

---
+++ final/glMagic.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <GL/gl.h>
+#include <GL/glut.h>
+class GLmagic{
+    public:
+    void alakazam()
+    {
+        /* Variables de luz */
+        GLfloat light_ambient[] = {0.0, 0.0, 0.0, 1.0};
+        GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+        GLfloat light_specular[] = {1.0, 1.0, 1.0, 1.0};
+        GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};
+
+        /* Variable de material */
+        GLfloat mat_ambient[] = {0.7, 0.7, 0.7, 1.0};
+        GLfloat mat_diffuse[] = {0.8, 0.8, 0.8, 1.0};
+        GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
+        GLfloat high_shininess[] = {100.0};
+
+        glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+        glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+        glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+    }
+    
+};
--- final/main.cpp
+++ final/main.cpp
@@ -8,6 +8,7 @@
 #include <math.h>
 #include "../glm/glm.h"
 #include "respuestas.h"
+#include "glMagic.h"
 /* Ya se incluyeron las librerias */
 
 /* Defines */
@@ -111,29 +112,7 @@ void drawCards(void);
 void drawSpades(void);
 void drawHearts(void);
 
-void GLMagic() {
-  /* Variables de luz */
-  GLfloat light_ambient[] = {0.0, 0.0, 0.0, 1.0};
-  GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
-  GLfloat light_specular[] = {1.0, 1.0, 1.0, 1.0};
-  GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};
 
-  /* Variable de material */
-  GLfloat mat_ambient[] = {0.7, 0.7, 0.7, 1.0};
-  GLfloat mat_diffuse[] = {0.8, 0.8, 0.8, 1.0};
-  GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
-  GLfloat high_shininess[] = {100.0};
-
-  glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-  glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-  glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-  glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-  glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-}
 
 /*
 void loadCrazyLivingRoom() {
@@ -756,7 +735,8 @@ int main(int argc, char **argv) {
   glutMotionFunc(Motion);
   glutIdleFunc(NULL);
 
-  GLMagic();
+  GLmagic moy;
+  moy.alakazam();
 
   if (lighting)
     glEnable(GL_LIGHTING);
